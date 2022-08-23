eb9466fbe630cf9a5b45b6d6f00a4baa012db9e1
unknown <hk32077@gmail.com>
2022-08-23 15:34:10 -0500

    Finished activity

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     }
 
   };
+   
   User.init({
     userId: {
       type: DataTypes.SMALLINT,
@@ -20,6 +21,13 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    role: {
+      type: DataTypes.ENUM,
+      values: [
+        'reviewer',
+        'admin',
+      ],
+    },
     passwordDigest: DataTypes.STRING
   }, {
     sequelize,
---
+++ backend/seeders/20210729202120-add-admin.js
@@ -0,0 +1,22 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [{
+      first_name: 'Hans',
+      last_name: 'Nielsen',
+      email: 'admin.m@example.com',
+      role: 'admin',
+      password_digest: await bcrypt.hash(process.env.ADMIN_PASSWORD, 10),
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', {
+      email: 'admin.m@example.com'
+    })
+  }
+}
